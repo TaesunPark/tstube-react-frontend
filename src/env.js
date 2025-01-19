4a1bbf64906bb2dea8cc0f9bf1c4e1ba27fbd967
taesunpark <tovbskvb@daum.net>
2025-01-20 02:11:03 +0900

    [chore] 도메인 주소 tstube 적용

--- src/env.js
+++ src/env.js
@@ -3,5 +3,10 @@
 // export const CHAT_SOCKET_URL = "ws://www.xn----bv7eq1qhzbe7i6wn.shop"
 
 /* 로컬 환경 */
-export const SERVER_URL = "http://localhost:8080";
-export const CHAT_SOCKET_URL = "ws://localhost:8080"
+// export const SERVER_URL = "http://localhost:8081";
+// export const CHAT_SOCKET_URL = "ws://localhost:8081";
+// export const MEDIA_SERVER_URL = "http://localhost:4321"
+
+export const SERVER_URL = "http://www.tstube.shop/api";
+export const CHAT_SOCKET_URL = "ws://www.tstube.shop";
+export const MEDIA_SERVER_URL = "http://www.tstube.shop:4321"
