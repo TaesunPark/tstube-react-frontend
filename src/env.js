53e4d2230aedda17953940245f4f712b82fdb3c9
taesunpark <tovbskvb@daum.net>
2025-03-25 09:56:06 +0900

    [chore] localhost -> tslocal change - Proxyman 에서 감지되게 변경 - mac os에서 localhost는 proxy 요청을 거치지 않음.

--- src/env.js
+++ src/env.js
@@ -3,17 +3,17 @@
 // export const CHAT_SOCKET_URL = "ws://www.xn----bv7eq1qhzbe7i6wn.shop"
 
 /* 로컬 환경 */
-// export const SERVER_URL = "http://localhost:8080/api/v1";
-// export const FILE_SERVER_URL = "http://localhost:8080";
-// export const CHAT_SOCKET_URL = "ws://localhost:8080";
-// export const MEDIA_SERVER_URL = "http://localhost:4321";
-// export const KAKAO_SERVER_URL = "http://www.tstube.shop/auth/login/kakao";
-// export const LOCAL_SERVER = "http://localhost:8080";
-export const PROD_SERVER = "https://www.tstube.shop";
-export const KAKAO_SERVER_URL = "https://www.tstube.shop/auth/login/kakao";
+export const SERVER_URL = "http://tslocal:8081/api/v1";
+export const FILE_SERVER_URL = "http://tslocal:8081";
+export const CHAT_SOCKET_URL = "ws://tslocal:8081";
+export const MEDIA_SERVER_URL = "http://tslocal:4321";
+export const KAKAO_SERVER_URL = "http://tslocal:8081/auth/login/kakao";
+export const LOCAL_SERVER = "http://tslocal:8081";
+// export const PROD_SERVER = "https://www.tstube.shop";
+// export const KAKAO_SERVER_URL = "https://www.tstube.shop/auth/login/kakao";
 
-export const SERVER_URL = "https://www.tstube.shop/api/v1";
-export const FILE_SERVER_URL = "https://www.tstube.shop";
-// export const CHAT_SOCKET_URL = "http://www.tstube.shop:8080";
-export const CHAT_SOCKET_URL = "wss://www.tstube.shop";
-export const MEDIA_SERVER_URL = "https://www.tstube.shop:4443"
+// export const SERVER_URL = "https://www.tstube.shop/api/v1";
+// export const FILE_SERVER_URL = "https://www.tstube.shop";
+
+// export const CHAT_SOCKET_URL = "wss://www.tstube.shop";
+// export const MEDIA_SERVER_URL = "https://ww  w.tstube.shop:4443"
